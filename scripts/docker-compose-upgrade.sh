b6fd46243bf450abeab774904f3cc991f967aafe
Simon <simonfong6@gmail.com>
2020-08-24 03:48:40 -0700

    Fail test and add echo

--- scripts/docker-compose-upgrade.sh
+++ scripts/docker-compose-upgrade.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 echo "Upgrading compose."
+path=$(pwd)
+echo "Path: $path"
 rm $(which docker-compose)
 curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
