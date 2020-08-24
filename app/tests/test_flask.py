8990daa60f30bf428393db6d0ca5a3cb1eaaf8d4
Simon <simonfong6@gmail.com>
2020-08-24 03:48:40 -0700

    Fail test and add echo

--- app/tests/test_flask.py
+++ app/tests/test_flask.py
@@ -28,7 +28,7 @@ def test_posts(client):
 
     # Plain seed data.
     data = response.get_json()
-    assert len(data) == 5
+    assert len(data) == 4
 
     # Add a post and see if it increases.
     response = client.get('/api/posts/new')
