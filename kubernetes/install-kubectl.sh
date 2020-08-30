41fae2162dc2b84990b6346f51d4a424d4f7967a
Simon <simonfong6@gmail.com>
2020-08-30 18:23:04 +0000

    Created helloworld kubernetes

--- kubernetes/install-kubectl.sh
+++ kubernetes/install-kubectl.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+<<<<<<< HEAD
 # https://kubernetes.io/docs/tasks/tools/install-kubectl/
 # Removed `sudo` because running as root.
 echo "Installing kubectl"
@@ -7,3 +8,10 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 apt-get install -y kubectl
+=======
+sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+>>>>>>> Created helloworld kubernetes
