d5ada4d474241970d991023188668a7cc4bfd778
Simon <simonfong6@gmail.com>
2020-08-31 23:55:31 -0700

    Use minikube docker daemon within buildkite

--- scripts/start-buildkite-agent.sh
+++ scripts/start-buildkite-agent.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
+# Use Minikube's Docker Daemon
+# Prefix all variables with `MINIKUBE_`
+eval $(minikube docker-env | sed 's/export /export MINIKUBE_/g')
+
+echo "Setting the following environment variables:"
+minikube docker-env | sed 's/export /export MINIKUBE_/g'
+
+# Start buildkite image.
 docker-compose -f docker-compose.buildkite.yml $@
