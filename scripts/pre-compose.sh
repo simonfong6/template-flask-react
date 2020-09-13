4d591d1779ba00f8b22ad8952bbaf53c2cfa39a2
Simon <simonfong6@gmail.com>
2020-09-13 16:24:54 -0700

    Removed unneeded scripts

--- scripts/pre-compose.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-export PROJECT_NAME="template-flask-react"
-
-export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source "$DIR/set-default-volume-path.sh"
--- scripts/set-default-volume-path.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-echo "VOLUME_PATH='$VOLUME_PATH'"
-
-if [[ -z "$VOLUME_PATH" ]]; then
-  echo "VOLUME_PATH=$VOLUME_PATH is empty, default to current directory."
-  export VOLUME_PATH="."
-  echo "VOLUME_PATH=$VOLUME_PATH"
-fi
