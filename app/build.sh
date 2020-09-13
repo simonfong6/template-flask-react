b9befa1675c714ab080738fb1c95980602db15bd
Simon <simonfong6@gmail.com>
2020-09-13 16:27:51 -0700

    Removed uneeded scrupts

--- app/build.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker build -t flask:0.5 .
--- app/test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-echo "Installing package locally."
-pip3 install ./app
-
-echo "Seeding database."
-python3 -m backend.database.seed
-
-echo "Running tests."
-pytest ./app/tests
